dd96aa3ff615765bb1edc5f59231db8099313ef3
jFranciscoZaina <jfranciscozaina@gmail.com>
2025-04-23 20:02:49 +0200

    Notion api/notion-proxy

--- api/hello.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-    res.status(200).json({
-      message: "¡Hola, Francisco! Tu API funciona correctamente 🚀",
-      fecha: new Date().toISOString(),
-    })
-  }
